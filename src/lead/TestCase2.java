38e24f517411fe976f878d340dec9b71e978c9c3
sagar <sagarmsd1397@gmail.com>
2023-01-31 12:54:10 +0530

    committing testcase2 to branchx

---
+++ src/lead/TestCase2.java
@@ -0,0 +1,15 @@
+package lead;
+
+public class TestCase2 
+{
+	public static void main(String[] args) 
+	{
+		String hungry = "Yes";
+		
+		if(hungry == "yes")
+			System.out.println("Eat something!");
+		else {
+			System.out.println("Do work");
+		}
+	}
+}
